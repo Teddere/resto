692268f63d0cee03fb51ddafb3db79d90bfb28bd
Teddere <teddermabanza@gmail.com>
2024-11-17 09:55:41 +0100

    config project

---
+++ resto_vue/README.md
@@ -0,0 +1,23 @@
+# resto_vue
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
+
+## This project has need backend interface
+
+For this project, i use django project
---
+++ resto_vue/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <nav>
+    <router-link to="/"> <i class="fa-solid fa-home"></i>Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </nav>
+  <router-view/>
+</template>
+
+<style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Playfair+Display:ital@0;1&family=Poppins:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600&display=swap');
+@import "~@fortawesome/fontawesome-free/css/all.min.css";
+@import "~bootstrap/dist/css/bootstrap.min.css";
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
